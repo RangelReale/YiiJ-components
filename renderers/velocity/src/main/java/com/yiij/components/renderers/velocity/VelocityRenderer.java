8efb4c940cc72769ed8c3ed7eb950d505c7fd959
Rangel Reale <rangelspam@gmail.com>
2012-02-12 23:54:30 -0200

    - Velocity renderer

---
+++ renderers/velocity/src/main/java/com/yiij/components/renderers/velocity/VelocityRenderer.java
@@ -0,0 +1,49 @@
+package com.yiij.components.renderers.velocity;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+
+import com.yiij.base.interfaces.IContext;
+import com.yiij.web.BaseController;
+import com.yiij.web.WebApplicationComponent;
+import com.yiij.web.interfaces.IPluginViewRenderer;
+
+public class VelocityRenderer extends WebApplicationComponent implements IPluginViewRenderer
+{
+	public VelocityRenderer(IContext context)
+	{
+		super(context);
+	}
+
+	@Override
+	public String getFileExtension()
+	{
+		return ".vm";
+	}
+
+	@Override
+	public String renderFile(BaseController controller, String file,
+			Object data, boolean doReturn) throws IOException
+	{
+		VelocityEngine ve = new VelocityEngine();
+		ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath"); 
+		ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+		ve.init();
+		
+		VelocityContext context = new VelocityContext();
+		context.put("data", data);
+		
+		Template t = ve.getTemplate(file);
+		
+		StringWriter writer = new StringWriter();
+		t.merge(context, writer);  
+		
+		return writer.toString();
+	}
+}
