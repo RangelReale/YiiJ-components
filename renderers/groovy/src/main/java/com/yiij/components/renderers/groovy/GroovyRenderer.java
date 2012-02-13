e7d7981877fd0e557f7add36181faa96cdc64c40
Rangel Reale <rangelspam@gmail.com>
2012-02-13 00:27:25 -0200

    - Groovy renderer

---
+++ renderers/groovy/src/main/java/com/yiij/components/renderers/groovy/GroovyRenderer.java
@@ -0,0 +1,44 @@
+package com.yiij.components.renderers.groovy;
+
+import groovy.lang.Writable;
+import groovy.text.SimpleTemplateEngine;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.yiij.base.interfaces.IContext;
+import com.yiij.web.BaseController;
+import com.yiij.web.WebApplicationComponent;
+import com.yiij.web.interfaces.IPluginViewRenderer;
+
+public class GroovyRenderer extends WebApplicationComponent implements IPluginViewRenderer
+{
+	public GroovyRenderer(IContext context)
+	{
+		super(context);
+	}
+
+	@Override
+	public String getFileExtension()
+	{
+		return ".groovy";
+	}
+
+	@Override
+	public String renderFile(BaseController controller, String file,
+			Object data, boolean doReturn) throws IOException
+	{
+		SimpleTemplateEngine engine = new SimpleTemplateEngine();
+		
+		Map<String, Object> makedata = new HashMap<String, Object>();
+		makedata.put("data", data);
+		
+		Writable template = engine.createTemplate(new BufferedReader(new InputStreamReader(Object.class.getResourceAsStream(file)))).
+				make(makedata);
+		
+		return template.toString();
+	}
+}
